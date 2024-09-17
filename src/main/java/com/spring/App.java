d4c86968f6a4bdb76953f770370e49fb891ef635
ankitrohila07 <111199878+ankitrohila07@users.noreply.github.com>
2024-09-18 00:01:42 +0530

    make basic structure of git

---
+++ src/main/java/com/spring/App.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Dev dev=(Dev)context.getBean("dev");
+//        dev.build();
+    }
+}
---
+++ src/main/java/com/spring/Dev.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+    public void build(){
+        System.out.println("working on Awesome Project");
+    }
+}
